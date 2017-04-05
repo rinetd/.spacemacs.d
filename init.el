8a453be865f6931006f3c89364996155b996030e
ubuntu <ubuntu@ubuntu.114.114.114.114>
2017-04-05 16:57:36 +0800

    zhexuan

--- init.el
+++ init.el
@@ -121,8 +121,8 @@ values."
 
      rinetd
 
-     (crypt :variables
-            crypt-gpg-key "rinetd")
+     ;; (crypt :variables
+     ;;        crypt-gpg-key "rinetd")
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
